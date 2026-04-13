1d0be5070ba03dc852812f9223c1c7a01e0954a4
Raphaell Yann <raphaellyann@hotmail.com>
2026-04-13 18:32:11 -0300

    Ajuste de configuracao e chaves

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  SUPABASE_URL: 'sb_publishable_M4PLX9AdwAGwoLCsjH_lHA_Nytdx8mH', // Insira a URL com https://
-  SUPABASE_KEY: 'sb_secret_frNd5HUN1WvzIF8nWwE8dQ_E9n0l_aj'        // Insira a chave pública
+  SUPABASE_URL: 'sb_publishable_M4PLX9AdwAGwoLCsjH_lHA_Nytdx8mH', 
+  SUPABASE_KEY: 'sb_secret_abpLVHIyzE6WaSEVOhQTFg_lW0MWhfd'   a     
 };
