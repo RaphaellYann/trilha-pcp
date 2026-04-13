b2fe3e37e3870ee4ee61e4f31b2362d45a7710bb
Raphaell Yann <raphaellyann@hotmail.com>
2026-04-13 18:29:37 -0300

    CONFIG

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  SUPABASE_URL: 'sb_publishable_M4PLX9AdwAGwoLCsjH_lHA_Nytdx8mH',
-  SUPABASE_KEY: 'sb_secret_IHYA3RlRQPQCMZk6MgVeyg_3yGLaZ3q'
+  SUPABASE_URL: 'sb_publishable_M4PLX9AdwAGwoLCsjH_lHA_Nytdx8mH', // Insira a URL com https://
+  SUPABASE_KEY: 'sb_secret_frNd5HUN1WvzIF8nWwE8dQ_E9n0l_aj'        // Insira a chave pública
 };
