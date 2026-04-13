f34f2483220a62c666d65d43ed9c8b872ae3332a
Raphaell Yann <raphaellyann@hotmail.com>
2026-04-13 18:48:45 -0300

    teste 3502564

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 const CONFIG = {
-  SUPABASE_URL: 'https://dsatibulgyntgbbgjg.supabase.co',
+  // Este é o link real do seu projeto que extraí dos seus prints
+  SUPABASE_URL: 'https://dsatibulgyntgbbgjg.supabase.co', 
+  
+  // Esta é a sua chave pública (Publishable)
   SUPABASE_KEY: 'sb_publishable_M4PLX9AdwAGwoLCsjH_lHA_Nytdx8mH'
 };
